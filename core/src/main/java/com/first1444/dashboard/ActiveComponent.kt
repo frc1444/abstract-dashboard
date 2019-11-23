08a938ab8fa00addd0086759d1ef27b05a185d06
Joshua Shannon <retrodaredevil@gmail.com>
2019-11-23 16:28:55 -0600

    Added VideoSourceSendable

--- core/src/main/java/com/first1444/dashboard/ActiveComponent.kt
+++ core/src/main/java/com/first1444/dashboard/ActiveComponent.kt
@@ -5,4 +5,15 @@ interface ActiveComponent {
     fun update()
 
     fun onRemove()
+
+    companion object {
+        @JvmStatic
+        fun createTitleOnly(title: String) = object : ActiveComponent {
+            override val title: String = title
+            override fun update() {
+            }
+            override fun onRemove() {
+            }
+        }
+    }
 }
---
+++ wpi/src/main/java/com/first1444/dashboard/wpi/VideoSourceSendable.kt
@@ -0,0 +1,15 @@
+package com.first1444.dashboard.wpi
+
+import com.first1444.dashboard.ActiveComponent
+import com.first1444.dashboard.BasicDashboard
+import com.first1444.dashboard.advanced.Sendable
+
+class VideoSourceSendable(
+        private val uri: String
+) : Sendable<ActiveComponent> {
+    override fun init(title: String, dashboard: BasicDashboard): ActiveComponent {
+        dashboard[".ShuffleboardURI"].strictSetter.setString(uri)
+        return ActiveComponent.createTitleOnly(title)
+    }
+
+}
